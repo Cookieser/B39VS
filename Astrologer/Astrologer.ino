ea60a1d0636fabfbff0c57517f2f53979bf99d33
Cookieser <324424398@qq.com>
2023-02-25 16:22:44 +0800

    Proteus 2.3

--- Astrologer/Astrologer.ino
+++ Astrologer/Astrologer.ino
@@ -1,139 +1,166 @@
-#include <TinyGPS++.h>
+/*Use some library*/
+#include <Wire.h> 
 #include <SoftwareSerial.h>
-#include <Keypad.h>
-#include <Wire.h> //调用wire库
-#include <LiquidCrystal_I2C.h> //调用LiquidCrystal_I2C库
-LiquidCrystal_I2C lcd(0x27,20,4); //设置LCD设备地址 
 
-String incomingByte = ""; //Declare variable with the type ofstring
-int degree= 10;
-int SensorPin = A0;
-int deflectionAngle;
-int elevation;
-float deflectionValue;
+#include <TinyGPS++.h> // The library which can process GPS data
+#include <Keypad.h> // The files about the operations of keypad
+#include <LiquidCrystal_I2C.h> //The library of LiquidCrystal_I2C
+
+/*Customize some classes*/
+//Packaged the functions of the motor as a class
+class Motor{
+private:    //Private attribute includes four pins and lastNum used for turning back
+  int Pin1;
+  int Pin2;
+  int Pin3;
+  int Pin4;
+  int lastNum;
+  
+public:
+  Motor(int pinFirst)//Constructor of this class
+  {
+    Pin1 = pinFirst;
+    Pin2 = pinFirst+1;
+    Pin3 = pinFirst+2;
+    Pin4 = pinFirst+3;
+    };
+
+  // Initialization of motor pins
+  void init()
+  {
+    for (int i = Pin1; i <= Pin4; i++) 
+        {
+          pinMode(i, OUTPUT);
+        }
+  }
+  
+  // Achieve a certain angle of clockwise rotation  
+  void clockwise(int num)
+  {
+    for (int count = 0; count < num; count++)
+        {
+          for (int i = Pin1; i <= Pin4; i++)
+              {
+                digitalWrite(i, HIGH);
+                delay(3);
+                digitalWrite(i, LOW);
+              }
+        }
+  lastNum = num; //Recoring this value for turning back
+}
+
+// Achieve a certain angle of anticlockwise rotation 
+void anticlockwise(int num)
+{
+  for (int count = 0; count < num; count++)
+      {
+        for (int i = Pin4; i >= Pin1; i--)
+            {
+              digitalWrite(i, HIGH);
+              delay(3);
+              digitalWrite(i, LOW);
+            }
+    }
+  lastNum = -num;//Recoring this value for turning back
+}
+
+//The motor can reset through this after rotation 
+void turnBack()
+{
+  if(lastNum<0)
+  {
+    clockwise(-lastNum);
+  }else
+  {
+    anticlockwise(lastNum);
+  }
+}
+
+};
 
 
-const byte rows = 4; //four rows
-const byte cols = 3; //three columns
+
+/*Setting of equipments and pins*/
+LiquidCrystal_I2C lcd(0x27,20,4); //Initialization of LCD by the device address to save pins
+
+int SensorPin = A0;//Sensor pin
+
+/*Setting of keypad*/
+const byte rows = 4; //Four rows
+const byte cols = 3; //Three columns
 char keys[rows][cols] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'},
   {'*','0','#'}
 };
-byte rowPins[rows] = {25, 24, 23, 22}; //连接行引脚
-byte colPins[cols] = {26, 27, 28}; //连接的列引脚
+byte rowPins[rows] = {25, 24, 23, 22}; //Row pins
+byte colPins[cols] = {26, 27, 28}; //Columns pin
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
-int scanValue(){
-  String inputValue="";
-   while(1){
-   char customKey = keypad.getKey();
-   
-  if (customKey){
-    
-    //Serial.println(customKey);
-   
-    if(customKey=='#') 
-    {
-      Serial.println(inputValue);
-      break;
-    
-    }
-    inputValue+=customKey;
-    
-     if(customKey=='*') {
-      lcd.setCursor(7,1);
-      lcd.print("                ");
-      inputValue="";
-      lcd.setCursor(7,1);
-      lcd.print(inputValue);
-      }
-      lcd.setCursor(7,1);
-      lcd.print(inputValue);
-  }
-   }
-   int res = atoi(inputValue.c_str());
-   return res;
-  }
 
-class Motor{
-private:    //私有成员，用来保存色彩的RGB分量
-  int Pin1;
-  int Pin2;
-  int Pin3;
-  int Pin4;
-  int lastNum=0;
-  
-
-public:
-  Motor(int pinFirst){
-    Pin1 = pinFirst;
-    Pin2 = pinFirst+1;
-    Pin3 = pinFirst+2;
-    Pin4 = pinFirst+3;
-    };
-  void clockwise(int num)
-{
-  for (int count = 0; count < num; count++)
-  {
-    for (int i = Pin1; i <= Pin4; i++)
-    {
-      digitalWrite(i, HIGH);
-      delay(3);
-      digitalWrite(i, LOW);
-    }
-  }
-  lastNum = num;
-}
-void anticlockwise(int num)
-{
-  for (int count = 0; count < num; count++)
-  {
-    for (int i = Pin4; i >= Pin1; i--)
-    {
-      digitalWrite(i, HIGH);
-      delay(3);
-      digitalWrite(i, LOW);
-    }
-  }
-  lastNum = -num;
-}
-
-void init(){
-    for (int i = Pin1; i <= Pin4; i++) {
-      pinMode(i, OUTPUT);
-    }
-  }
-void turnBack(){
-  if(lastNum<0){
-    clockwise(-lastNum);
-    
-    }else{
-      anticlockwise(lastNum);
-      }
-  }
-
-};
-
-
-
+/*Setting of motors*/
 Motor *motorA = new Motor(10);
 Motor *motorB = new Motor(6);
 Motor *motorC = new Motor(2);
 
-
+/*Setting of GPS module*/
 const int RXPin = 99, TXPin = 100;
-const uint32_t GPSBaud = 9600; //Default baud of NEO-6M is 9600
-
-
+const uint32_t GPSBaud = 9600; //Default baud is 9600
 TinyGPSPlus gps; // the TinyGPS++ object
 
+/*Variable declaration*/
+int deflectionAngle;
+int elevation;
+float deflectionValue;
+String incomingByte = ""; //Declare variable with the type of string
+int degree= 10;
+
+
+
+/*Customize some functions*/
+
+//Get the number from keypad
+int scanValue(){
+   String inputValue = "";
+   while(1)
+   {
+      char customKey = keypad.getKey();//get an input char
+      if (customKey)
+      {
+        if(customKey == '#') // Similar to the Confirm key
+          {
+            Serial.println(inputValue);
+            break;
+          }
+        inputValue += customKey; // Splicing chars
+    
+        if(customKey == '*') // Similar to the Clear key
+          {
+            lcd.setCursor(7,1);
+            lcd.print("                ");
+            inputValue = "";
+            lcd.setCursor(7,1);
+          }
+        lcd.setCursor(7,1);
+        lcd.print(inputValue);//Show data in the lcd screen
+     }
+   }
+   int res = atoi(inputValue.c_str());// Data type conversion from STRING to INT 
+   return res;
+  }
+
+
+
+
+//GPS data processing
 void gpsFunction(){
-  
-  if (Serial1.available() > 0) {
-    if (gps.encode(Serial1.read())) {
-      if (gps.location.isValid()) {
-        Serial.print(F("- latitude: "));
+  if (Serial1.available() > 0) //Receive the GPS information
+  {
+    if (gps.encode(Serial1.read()))//Read the GPS information
+    {
+      if (gps.location.isValid()) //Judge  whether the location data is valid
+      {
+        Serial.print(F("- latitude: "));//Show some info to upper computer
         Serial.println(gps.location.lat());
 
         Serial.print(F("- longitude: "));
@@ -141,15 +168,16 @@ void gpsFunction(){
 
         Serial.print(F("- altitude: "));
         if (gps.altitude.isValid())
-          Serial.println(gps.altitude.meters());
+            Serial.println(gps.altitude.meters());
         else
-          Serial.println(F("Measuring..."));
+            Serial.println(F("Measuring..."));
       } else {
         Serial.println(F("- location: Measuring..."));
       }
 
       Serial.print(F("- speed: "));
-      if (gps.speed.isValid()) {
+      if (gps.speed.isValid()) //Judge whether the speed data is valid
+      {
         Serial.print(gps.speed.kmph());
         Serial.println(F(" km/h"));
       } else {
@@ -157,7 +185,8 @@ void gpsFunction(){
       }
 
       Serial.print(F("- GPS date&time: "));
-      if (gps.date.isValid() && gps.time.isValid()) {
+      if (gps.date.isValid() && gps.time.isValid()) //Judge whether the time data is valid
+      {
         Serial.print(gps.date.year());
         Serial.print(F("-"));
         Serial.print(gps.date.month());
@@ -177,63 +206,65 @@ void gpsFunction(){
     }
   }
 
-  if (millis() > 5000 && gps.charsProcessed() < 10)
+  if (millis() > 5000 && gps.charsProcessed() < 10) // Error prompt
     Serial.println(F("No GPS data received: check wiring"));}
 
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
   
-  lcd.init();                  // 初始化LCD_1602A
-  lcd.backlight();             //设置LCD背景等亮
+  //Serial port setting
+  Serial.begin(9600);
+  Serial1.begin(9600);//Use for GPS module
+  
+  //LCD sets up and shows some info
+  lcd.init();                  
+  lcd.backlight();            
   lcd.setCursor(0,0);
   lcd.print("Welcome!!!");
   lcd.setCursor(0,1);
   lcd.print("GPS is activating...");
   lcd.setCursor(0,2);
   lcd.print("Waiting...");
+
+  //Motors starts to activate
   motorA->init();
   motorB->init();
   motorC->init();
-  /*
+
+  //GPS activates and send messages to LCD when it has finished
   while(1){
   gpsFunction();
-  
-  if(gps.location.lat()){
-  lcd.setCursor(0,0);  
-  lcd.print("GPS module is ready");
-  lcd.setCursor(0,1);
-  lcd.print("- latitude: ");
-  lcd.print(gps.location.lat());
-  lcd.print("   ");
-  lcd.setCursor(0,2);
-  lcd.print("- longitude: ");
-  lcd.print(gps.location.lng());
-  lcd.setCursor(0,2);
-  lcd.print("Waiting for commands");
-  break;
-    
+  if(gps.location.lat())
+  {
+    //Send messages
+    lcd.setCursor(0,0);  
+    lcd.print("GPS module is ready");
+    lcd.setCursor(0,1);
+    lcd.print("- latitude: ");
+    lcd.print(gps.location.lat());
+    lcd.print("   ");
+    lcd.setCursor(0,2);
+    lcd.print("- longitude: ");
+    lcd.print(gps.location.lng());
+    lcd.setCursor(0,2);
+    lcd.print("Waiting for commands");
+    break;
     }
   }
-  */
-
 }
 
 
 void loop() {
- 
 while (Serial.available() > 0)
  {
-     incomingByte += char(Serial.read());//读取单个字符值，转换为字符，并按顺序一个个赋值给incomingByte
-     delay(10);//不能省略，因为读取缓冲区数据需要时间
- }
+     incomingByte += char(Serial.read());//Read the single character value, convert to char and assign to incomingByteRead one by one in order
+     delay(10);//This cannot be ignored because reading buffer data needs time
 
 //The response of the specific component accordingly to the prescribed command  
 
  if ( incomingByte.length() > 0 ) 
  { 
-  //初始化与系统自测
+  //Self Test & Init
    if( incomingByte == "c") {
     lcd.clear();
     lcd.setCursor(0,0);
@@ -317,7 +348,7 @@ while (Serial.available() > 0)
     lcd.print("-m :Self-adaption");
     lcd.setCursor(0,2);
     lcd.print("-r :Tracking Star");
-    incomingByte = "";//清空变量，准备下次输入
+    incomingByte = "";//Clear the variables for the next input
    }
   
   
@@ -328,4 +359,5 @@ while (Serial.available() > 0)
   delay(500);
 
     
+}
 }
